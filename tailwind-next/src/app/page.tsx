ac16b81298191c77fc63b8bd80b8568f34b654f5
Fabio Batoni <fabio_batoni@hotmail.com>
2024-01-10 13:13:38 +0000

    adicionado projeto `next` dentro do repositorio

---
+++ tailwind-next/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl text-fabio">Hello Tailwind!</h1>
+      <h1>Hello Tailwind!</h1>
+      <h1>Hello Tailwind!</h1>
+    </div>
+  );
+}
